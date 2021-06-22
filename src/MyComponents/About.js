5d9c6e5bdff62dbd639f5f218eccad4443e6c2ab
Happy <Gouravnama1101@gmail.com>
2021-06-22 17:21:31 +0530

    First Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            
+            <p> Hi, I am Gourav and this is my personal To-do's list.</p>
+        </div>
+    )
+}
